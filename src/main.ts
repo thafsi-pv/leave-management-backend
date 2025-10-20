8de8e2321d05e892c6d00390f34332899c391504
Thafsi PV <pvthafsi@gmail.com>
2025-10-20 10:59:06 +0530

    fix:cors frontend url

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   }));
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://leave-management-backend-118y.onrender.com'],
+    origin: ['http://localhost:3000', 'https://leave-management-frontend-ebon.vercel.app/'],
     credentials: true,
   });
 
